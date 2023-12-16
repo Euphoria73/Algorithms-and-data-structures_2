465ba5b48095332f6c73194a45f588097261b609
Euphoria73 <59795173+Euphoria73@users.noreply.github.com>
2023-12-16 10:32:24 +0400

    Add files via upload

---
+++ CoinsTask.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace LabWork_2
+{
+    public static class CoinsTask
+    {
+        public static int[] GreedyAlg(int amount, int[] coins)
+        {
+            Array.Sort(coins);
+            Array.Reverse(coins);
+            List<int> result = new List<int>();
+
+            foreach (var item in coins)
+            {
+                while (amount >= item)
+                {
+                    amount -= item;
+                    result.Add(item);
+                }
+            }
+            return result.ToArray();
+        }
+
+        public static int[] DynamicAlg(int[] coins, int amount)
+        {            
+            int[] dp = new int[amount + 1];
+            dp[0] = 0;
+            // Меняем каждую монету, сохраняя решение подзадачи для дальнейшего использования, то есть заполняем таблицу
+            for (int coin = 1; coin <= amount; coin++)
+            {
+                // Когда монета с наименьшим значением используется для сдачи, количество требуемых монет является наибольшим
+                int minCoins = coin;
+                // просматриваем каждую монету достоинства, чтобы увидеть, можно ли ее использовать как одну из сдач
+                for (int kind = 0; kind < coins.Length; kind++)
+                {
+                    // Если текущий номинал монеты меньше текущих монет, проходим по предыдущим значениям таблицы
+                    if (coins[kind] <= coin)
+                    {
+                        int temp = dp[coin - coins[kind]] + 1;
+                        if (temp < minCoins)
+                        {
+                            minCoins = temp;
+                        }
+                    }
+                }
+                // Сохраняем минимальное количество монет
+                dp[coin] = minCoins;
+            }
+            return dp;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace LabWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] coins = { 1,3,4 }; //проверка на числе 6
+            Console.WriteLine("Набор монет: ");
+            PrintItems(coins);
+            
+            Console.WriteLine("\nВведите сумму которую хотите разменять:");
+            int amount = Convert.ToInt32(Console.ReadLine());
+           
+            int[] greedyResult = CoinsTask.GreedyAlg(amount, coins);
+
+            Console.WriteLine("Число монет в решении жадного алгоритма - " + greedyResult.Length);
+            Console.WriteLine("Номиналами: ");
+            PrintItems(greedyResult);
+
+            int[] dynamicResult = CoinsTask.DynamicAlg(coins, amount);            
+            Console.WriteLine("\nЧисло монет в решении динамическим программированием: " + dynamicResult[^1]);
+            
+            Console.ReadKey();           
+        }
+        static void PrintItems(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
